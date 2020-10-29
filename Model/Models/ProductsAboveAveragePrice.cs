648685d87d272e1def187bde99b1ae24f89fbed5
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-10-29 10:30:31 +0100

    Added models

---
+++ Model/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Models
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
---
+++ Model/Models/SummaryOfSalesByQuarter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Models
+{
+    public partial class SummaryOfSalesByQuarter
+    {
+        public DateTime? ShippedDate { get; set; }
+        public int OrderId { get; set; }
+        public decimal? Subtotal { get; set; }
+    }
+}
